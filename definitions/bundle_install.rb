488abc1e166dafa37bfda446ac0d18a3a3bfc6be
Ross Bragg <ross@perfectsensedigital.com>
2016-03-21 15:14:43 -0400

    Add bundler to chef, bundle with the chef-bundler

--- definitions/bundle_install.rb
+++ definitions/bundle_install.rb
@@ -1,11 +1,11 @@
 define :bundle_install do
   # install bundler gem
-  gem_package 'bundler' do
+  chef_gem 'bundler' do
     options '--no-ri --no-rdoc'
   end
 
   # bundle install
-  execute 'bundle install' do
+  execute '/opt/chef/embedded/bin/bundle install' do
     cwd params[:path]
     command "bundle install"
     user params[:user]
